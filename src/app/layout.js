1209063c8c4d47e22d4bacb713aae6bcc5223760
AdriianEnriquez <nunoelpuma@gmail.com>
2025-11-26 21:20:26 -0600

    fixViews

--- src/app/layout.js
+++ src/app/layout.js
@@ -42,6 +42,7 @@ export default function RootLayout({ children }) {
                 },
               }}
             />
+            
             {children}
           </DataProvider>
         </AuthProvider>
